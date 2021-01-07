5a17743ab5bdfcef32567a8e7029438569d59008
XueDee123 <1411719639@qq.com>
2021-01-07 10:25:17 +0800

    day09

---
+++ win32练习/day01/Windows数据类型/Windows数据类型/Windows数据类型.cpp
@@ -0,0 +1,23 @@
+﻿
+#include<Windows.h>
+#include <iostream>
+
+void test()
+{
+    LPSTR str =(LPSTR) "这个是windows字符串类型";
+    std::cout << str << std::endl;
+    CHAR lpstr[120] = "这是windows字符数组类型";
+    std::cout << lpstr << std::endl;
+    INT a = 100;
+    DWORD d = 0xffff;
+    MessageBoxA(NULL, str, "LPSTR", MB_OK);
+
+}
+
+int main()
+{
+    test();
+    std::cout << "Hello World!\n";
+    return 0;
+}
+
